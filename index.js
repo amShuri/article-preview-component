46504136d8904eb5ec9b04e9557fa2d03d898ac4
amShuri <106229595+amShuri@users.noreply.github.com>
2026-03-07 15:54:46 -0300

    refactor: replace share links with buttons and add focus styles

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const btn = document.getElementById('share-button');
 const shareIcons = document.getElementById('share-icons');
+const firstIcon = document.getElementById('first-icon');
 
 btn.addEventListener('click', (e) => {
   btn.classList.toggle('share__button--dark');
   shareIcons.classList.toggle('hidden');
+
+  firstIcon.focus();
 });
